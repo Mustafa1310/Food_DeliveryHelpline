6a331db4b74ada438fbaa12f6edae38f3fb25771
Mustafa Malik <58935609+Mustafa1310@users.noreply.github.com>
2021-05-23 23:12:40 +0530

    Add files via upload

---
+++ FoodDelivery/connect.php
@@ -0,0 +1,23 @@
+<?php
+	$firstName = $_POST['firstName'];
+	$Complaint = $_POST['Complaint'];
+	$gender = $_POST['gender'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$number = $_POST['number'];
+
+	// Database connection
+	$conn = new mysqli('localhost','root','','test');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into registration(firstName, Complaint, gender, email, password, number) values(?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("sssssi", $firstName, $Complaint, $gender, $email, $password, $number);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+	}
+?>
